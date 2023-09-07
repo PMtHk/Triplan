f006c8b61dff367d13305aaaa929bae9d6b1854a
zooby <zooby88@naver.com>
2023-09-07 13:56:41 +0900

    chore : Set up project

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,32 @@
+import '../globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'TriPlan',
+  description: 'Go on a trip right now',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>
+        {/* TODO: TopNavbar */}
+        <main className='container'>
+          {/* TODO: LeftSidebar */}
+          <div className='w-full'>
+            {children}
+          </div>
+          {/* TODO: LeftSidebar */}
+        </main>
+        {/* TODO: BottomNavbar */}
+      </body>
+    </html>
+  )
+}
